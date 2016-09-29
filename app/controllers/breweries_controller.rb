a08bfe03ec81a64d00c43d714b386411f71d8c8f
DO Alex <ETD-P\do@i22-13-lx.etd-p.esiea.fr>
2016-09-29 09:20:03 +0200

    sdfsdfdsf

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -69,6 +69,6 @@ class BreweriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
-      params.require(:brewery).permit(:name, :description)
+      params.require(:brewerie).permit(:name, :description)
     end
 end
--- app/models/brewerie.rb
+++ app/models/brewerie.rb
@@ -1,2 +1,3 @@
 class Brewerie < ActiveRecord::Base
+	has_many :beers
 end
